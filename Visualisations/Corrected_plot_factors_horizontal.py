ffb2ae73f62bdc467553defef6ec64b1d7e0a811
foroveralls <jeverallp@gmail.com>
2024-06-25 20:02:40 +0200

    updating figures

--- Visualisations/Corrected_plot_factors_horizontal.py
+++ Visualisations/Corrected_plot_factors_horizontal.py
@@ -6,6 +6,8 @@ import numpy as np
 # File paths
 filepath = "../Data/Compiled/The Pareto effect in tipping social networks Tipping Data - Tipping_Data.tsv"
 
+palette = sns.color_palette("husl", 2)
+
 def to_numeric_magnitude(value):
     try:
         return float(value)
@@ -36,7 +38,8 @@ def preprocess_data(df):
     effect_magnitude_data['symbol'] = effect_magnitude_data.apply(magnitude_to_symbol, axis=1)
     
     effect_cascade_data['plotting_variable'] = effect_cascade_data['grouping_term'].combine_first(effect_cascade_data['variable'])
-    effect_cascade_data['colors'] = effect_cascade_data['effect_cascade_success'].map({'+': 'g', '-': 'r'}).fillna('b')
+    
+    effect_cascade_data['colors'] = effect_cascade_data['effect_cascade_success'].map({'+': palette[1], '-': palette[0], '-/+': 'purple'}).fillna('gray')
     
     count_data = effect_cascade_data.groupby(['plotting_variable', 'effect_cascade_success']).size().reset_index(name='counts')
     effect_cascade_data = effect_cascade_data.merge(count_data, on=['plotting_variable', 'effect_cascade_success'], how='left')
@@ -60,10 +63,13 @@ def preprocess_data(df):
 def plot_effect_cascade_success(effect_cascade_data, size_mapping_legend, ax):
     sns.set(style="whitegrid")
     
+    # Reverse the color mapping
+    color_mapping = {'+': palette[1], '-': palette[0], '-/+': 'purple', 'n': 'gray'}
+    
     sns.scatterplot(x="effect_cascade_success", y="plotting_variable", 
                     size="size_category", sizes=size_mapping_legend, 
                     data=effect_cascade_data, ax=ax, legend="full", 
-                    hue='colors', palette={'g': 'g', 'r': 'r', 'b': 'b'})
+                    hue='effect_cascade_success', palette=color_mapping)
     
     handles, labels = ax.get_legend_handles_labels()
     size_handles = handles[-4:]
@@ -73,15 +79,16 @@ def plot_effect_cascade_success(effect_cascade_data, size_mapping_legend, ax):
     ax.yaxis.set_tick_params(pad=5)
     ax.set_xlabel('Measure of effect', fontsize=16)
     ax.set_ylabel('', fontsize=16)
-    ax.set_xlim(-0.5, 1.5)
+    ax.set_xlim(-0.6, 1.6)
     ax.tick_params(axis='both', which='major', labelsize=16)
 
 def plot_effect_magnitude(effect_magnitude_data, ax):
+   
     sns.set(style="whitegrid")
     variables = effect_magnitude_data['variable'].unique()
     symbols = {'+': 0, '-': 1, '±': 2}
-    y_positions = {variable: i * 3 for i, variable in enumerate(variables)}  # Increase spacing between y-ticks
-    color_mapping = {'+': 'g', '-': 'r', '±': 'b'}
+    y_positions = {variable: i * 3 for i, variable in enumerate(variables)}
+    color_mapping = {'+': palette[1], '-': palette[0], '±': 'gray'}
     size_mapping = {1.0: 50, 2.0: 100, 3.0: 150}
     x_jitter = 0.2
     y_jitter = 0
@@ -90,7 +97,7 @@ def plot_effect_magnitude(effect_magnitude_data, ax):
         y = y_positions[row['variable']] + np.random.uniform(-y_jitter, y_jitter)
         x = symbols.get(row['symbol'], 2) + np.random.uniform(-x_jitter, x_jitter)
         size = size_mapping.get(row['numeric_magnitude'], 50)
-        color = color_mapping.get(row['symbol'], 'b')
+        color = color_mapping.get(row['symbol'], 'gray')
         sns.scatterplot(x=[x], y=[y], size=[size], sizes=(size, size), alpha=0.8, color=color, marker='D', edgecolor='w', linewidth=0.5, ax=ax)
     
     ax.set_yticks(list(y_positions.values()))
@@ -116,20 +123,32 @@ def main():
     dot_sizes = [50, 150, 300, 500]
     size_mapping_legend = {label: size for label, size in zip(labels, dot_sizes)}
     
-    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 7), gridspec_kw={'width_ratios': [1, 0.8], 'wspace': 0.1})
+    fig = plt.figure(figsize=(12, 8), constrained_layout=True)
+    gs = fig.add_gridspec(1, 2, width_ratios=[0.55, 0.45])
+    
+    ax1 = fig.add_subplot(gs[0])
+    ax2 = fig.add_subplot(gs[1])
+    
     plot_effect_cascade_success(effect_cascade_data, size_mapping_legend, ax1)
     plot_effect_magnitude(effect_magnitude_data, ax2)
     
-    # Draw vertical line between plots
-    line = plt.Line2D((0.55, 0.55), (0.1, 0.9), color='black', linewidth=2, transform=fig.transFigure, figure=fig)
+    # Adjust the subplot layouts
+    ax1.set_xlim(-0.6, 1.6)
+    ax2.set_xlim(-0.5, 2.5)
+    
+    line = plt.Line2D((0.625, 0.625), (0.1, 0.9), color='black', linewidth=1, transform=fig.transFigure, figure=fig)
     fig.add_artist(line)
     
-    # Add (a) and (b) labels
-    ax1.text(0.5, 1.1, '(a)', transform=ax1.transAxes, fontsize=16, fontweight='bold', va='top', ha='right')
-    ax2.text(0.5, 1.1, '(b)', transform=ax2.transAxes, fontsize=16, fontweight='bold', va='top', ha='right')
+    # Adjust the position and size of the axes
+    ax1.set_position([0.25, 0.1, 0.35, 0.8])
+    ax2.set_position([0.65, 0.1, 0.35, 0.8 ])
     
-    plt.tight_layout()
-    plt.savefig("../Figures/Combined_plot_final.png", dpi=300, bbox_inches="tight")
+    # Add (a) and (b) labels
+    ax1.text(0.02, 1.02, '(a)', transform=ax1.transAxes, fontsize=16, fontweight='bold', va='bottom', ha='left')
+    ax2.text(0.02, 1.02, '(b)', transform=ax2.transAxes, fontsize=16, fontweight='bold', va='bottom', ha='left')
+    
+    # Save in SVG format
+    plt.savefig("../Figures/Combined_plot_final.svg", format='svg', bbox_inches="tight")
     plt.show()
 
 if __name__ == "__main__":
